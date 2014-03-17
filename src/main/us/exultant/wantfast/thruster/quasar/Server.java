72c6d0ac6a270a6a11a04e6e3a531afa553e4501
Eric Myhre <hash@exultant.us>
2014-03-16 21:26:28 -0500

    mention the contact points between quasar and network.  going to back up and make a more rigourous arrangement of client-able-to-hit-server and unify thruster interfaces before going much farther.

---
+++ src/main/us/exultant/wantfast/thruster/quasar/Server.java
@@ -0,0 +1,10 @@
+package us.exultant.wantfast.thruster.quasar;
+
+import co.paralleluniverse.fibers.io.*;
+
+public class Server {
+	// see FibersMXBean and/or implmenting a FibersMonitor for inspection of schedulers.
+	// see FiberExecutorScheduler for using existing/legacy executor pools instead of new FJPools.
+
+	FiberServerSocketChannel sock;
+}
